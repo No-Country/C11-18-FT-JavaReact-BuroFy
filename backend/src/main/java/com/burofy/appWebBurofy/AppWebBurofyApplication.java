1c438883b0af33a913e22fb8dffa29289f8dc9e9
LilianaGallego <lilianagallegom@gmail.com>
2023-05-15 10:33:08 -0500

    "Se crea la estructura base del proyecto con https://start.spring.io/"

---
+++ backend/src/main/java/com/burofy/appWebBurofy/AppWebBurofyApplication.java
@@ -0,0 +1,13 @@
+package com.burofy.appWebBurofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebBurofyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebBurofyApplication.class, args);
+	}
+
+}
