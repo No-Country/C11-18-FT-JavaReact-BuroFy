e72c6d1f1abadc60980604cdd81984e6658a222e
Dmitry Borovskikh <dimantos@me.com>
2023-05-17 19:07:03 +0300

    cambios

--- backend/src/main/java/com/burofy/appWebBurofy/entities/Person.java
+++ backend/src/main/java/com/burofy/appWebBurofy/entities/Person.java
@@ -1,22 +1,7 @@
 package com.burofy.appWebBurofy.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import javax.persistence.*;
-
-@Entity
-@Table(name = "person")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public abstract class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String name;
     private String lastName;
     private String documentNumber;
