11662268107f2487ffd609917fb44fa2c85522bd
Afrozens <jesuszen2.6@gmail.com>
2023-05-17 18:05:24 -0400

    feat: upload components and firebase services

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontSize: {
+        "primary": "36px",
+        "secundary": "30px",
+        "tertiary": "25px",
+      },
+      fontFamily: {
+        "DM-primary": 'DM-Sans',
+      },
+      backgroundColor: {
+        "primary": "#C600E0",
+        "secundary": "#7FCEFF",
+        "tertiary": "#2E2E2E",
+        "quaternary": "#FFFFFF",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'header-img': "url('/bg-header-burofy.svg')",
+      },
+      colors: {
+        lilac: "#C600E0",
+        "sky-blue": "#7FCEFF",
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
-}
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/forms")],
+};
