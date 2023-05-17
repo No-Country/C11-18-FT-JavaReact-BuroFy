24d5f4d574ec43614383aae3b79b5a5e2ff5e531
Milton Figueredo Miles <gratno@gmail.com>
2023-05-17 11:10:52 -0500

    Completando Payment and Client

--- backend/src/main/java/com/burofy/appWebBurofy/Entidades/Client.java
+++ backend/src/main/java/com/burofy/appWebBurofy/Entidades/Client.java
@@ -1,5 +1,24 @@
+package com.burofy.appWebBurofy.Entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 
-public class Client extends Persona {
+@Entity
+@Table (name = "client")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client extends Person {
+
+    private Long id;
+    private String occupation ;
+
     
 }
