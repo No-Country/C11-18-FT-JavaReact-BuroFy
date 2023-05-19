edac517b9f74ba97203f36dfd1bc94eae2461221
LilianaGallego <lilianagallegom@gmail.com>
2023-05-19 11:05:29 -0500

    build: Se modifica la estructura de carpetas para la creación de un cliente

---
+++ backend/src/main/java/com/burofy/appWebBurofy/utility/Response.java
@@ -0,0 +1,21 @@
+package com.burofy.appWebBurofy.utility;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+public class Response {
+
+    protected HttpStatus status;
+    protected String message;
+    protected Optional data;
+
+}
