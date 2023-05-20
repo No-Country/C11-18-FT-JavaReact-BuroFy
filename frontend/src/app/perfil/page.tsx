280ba64b40703377bd9c62faea4bdc78c37891a4
jorge4684 <js5383651@gmail.com>
2023-05-19 18:37:24 -0600

    perfil page1

--- frontend/src/app/perfil/page.tsx
+++ frontend/src/app/perfil/page.tsx
@@ -1,9 +1,13 @@
+
+import PerfilOne from "@/components/Perfil/PerfilOne";
+import PerfilTwo from "@/components/Perfil/PerfilTwo";
 import React from "react";
+
 
 const ProfilePage = () => {
   return (
     <main>
-      <h2>Hola Perfil</h2>
+      <h2><PerfilOne/></h2>
     </main>
   );
 };
