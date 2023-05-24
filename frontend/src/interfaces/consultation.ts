14b8375759fc9716d90cec5309ae97682e0d7e40
Eugenia Gramajo <110125413+EugeniaGramajo@users.noreply.github.com>
2023-05-24 12:35:31 -0300

    Revert "Rama juje"

--- frontend/src/interfaces/consultation.ts
+++
@@ -1,9 +0,0 @@
-export interface Consultation {
-  id: string | number;
-  idClient: string | number;
-  idProfessional: string | number;
-  idPay: string | number;
-  type: string;
-  date: Date;
-  comments: string;
-}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,52 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      fontSize: {
-        primary: "36px",
-        secundary: "30px",
-        tertiary: "25px",
-      },
-      fontFamily: {
-        "primary-roboto": "Epilogue",
-      },
-      backgroundColor: {
-        primary: "#C600E0",
-        secundary: "#7FCEFF",
-        tertiary: "#2E2E2E",
-        quaternary: "#FFFFFF",
-        quinary: "#D9D9D9",
-        sixnary: "#F0BFF7"
-      },
       backgroundImage: {
-        "header-img": "url('/bg-header-burofy.svg')",
-      },
-      colors: {
-        lilac: "#C600E0",
-        "sky-blue": "#7FCEFF",
-      },
-      animation: {
-        "spinner-square": "spinner 3s infinite",
-      },
-      keyframes: {
-        spinner: {
-          "0%": { transform: "rotate(-45deg)" },
-          "50%": {
-            transform: "rotate(-360deg)",
-            "border-radius": "50%",
-          },
-          "100%": {
-            transform: "rotate(-45deg)",
-          },
-        },
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
-};
+  plugins: [require('flowbite/plugin')],
+}
