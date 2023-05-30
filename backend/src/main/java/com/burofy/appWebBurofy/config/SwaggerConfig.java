bb711155cfe8e559ba50963eddab741720f7e49a
LilianaGallego <lilianagallegom@gmail.com>
2023-05-30 11:30:35 -0500

    buidl: "Se implementa la documentación con swagger"

--- backend/src/main/java/com/burofy/appWebBurofy/config/SwaggerConfig.java
+++ backend/src/main/java/com/burofy/appWebBurofy/config/SwaggerConfig.java
@@ -1,29 +1,40 @@
-//package com.burofy.appWebBurofy.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiKey;
-//import springfox.documentation.service.AuthorizationScope;
-//import springfox.documentation.service.SecurityReference;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spi.service.contexts.SecurityContext;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import java.util.Arrays;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//        @Bean
-//        public Docket api() {
-//            return new Docket(DocumentationType.SWAGGER_2)
-//                    .select()
-//                    .apis(RequestHandlerSelectors.basePackage("com.burofy.appWebBurofy"))
-//                    .paths(PathSelectors.any())
-//                    .build();
-//        }
-    //}
+package com.burofy.appWebBurofy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Spring Boot Burofy API",
+                "Buryofy API REST",
+                "Version: 1.0",
+                "Despliegue: https://backend-web-burofy.onrender.com",
+                new Contact(
+                        "Burofy",
+                        "https://github.com/No-Country/C11-18-FT-JavaReact-BuroFy.git", "burofy@example.com"),
+                "MIT",
+                "url",
+                Collections.emptyList());
+    }
+
+}
 
--- backend/src/main/java/com/burofy/appWebBurofy/security/config/Config.java
+++
@@ -1,4 +0,0 @@
-package com.burofy.appWebBurofy.security.config;
-
-//public class Config {
-//}
