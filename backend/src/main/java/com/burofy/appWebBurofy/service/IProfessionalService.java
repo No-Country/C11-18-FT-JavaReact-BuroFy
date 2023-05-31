40a239cde229d03352db1e46b6fc16c63fd82861
LilianaGallego <lilianagallegom@gmail.com>
2023-05-31 09:32:33 -0500

    build: "Se implementa el consultar todos los profesionales con paginación"

--- backend/src/main/java/com/burofy/appWebBurofy/service/IProfessionalService.java
+++ backend/src/main/java/com/burofy/appWebBurofy/service/IProfessionalService.java
@@ -1,15 +1,15 @@
 package com.burofy.appWebBurofy.service;
 
-
-
-import com.burofy.appWebBurofy.entity.Client;
 import com.burofy.appWebBurofy.entity.Professional;
+
+import java.util.List;
 
 public interface IProfessionalService {
 
     Professional createProfessional(Professional professional);
 
     Professional getProfessional(Long id);
+    List<Professional> allProfessionals(int page, int pageSize);
 
     Professional updateProfessional(Long id, Professional updatedProfessional);
 
