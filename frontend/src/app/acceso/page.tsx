09d9e158359df9be1cbed99c04c6fdedb9fe6580
“agtelo” <alantelo1987@gmail.com>
2023-06-03 14:31:13 -0300

    fix: layout styles on tailwind cfg

--- frontend/src/app/acceso/page.tsx
+++ frontend/src/app/acceso/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function Login() {
   return (
-    <section className=' w-full flex flex-col items-center px-4 pt-10 bg-white md:p-24 lg:p-0 lg:flex-row lg:justify-between lg:items-start overflow-hidden lg:top-0 lg:left-0 lg:absolute'>
+    <section className='layout-out'>
       <FormLogin />
       <Heroimage />
     </section>
--- frontend/src/app/registro/cliente/page.tsx
+++ frontend/src/app/registro/cliente/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 
 const registerClient = () => {
   return (
-    <section className='w-full flex flex-col items-center px-4 pt-10 bg-white md:p-24 lg:p-0 lg:flex-row lg:justify-between lg:items-start overflow-hidden absolute top-0 left-0  z-50'>
+    <section className='layout-out'>
       <FormRegisterClient />
       <Heroimage />
     </section>
